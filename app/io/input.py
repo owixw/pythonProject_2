b36fcf956eee63eebefb2e43fc82e23d7ed7d392
Mariia Andrianova <owixw1@gmail.com>
2024-03-16 20:11:07 +0200

    Functions added

---
+++ app/io/input.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+
+def input_from_console():
+   pass
+
+
+def read_from_file(file_path):
+    pass
+
+
+def read_from_file_using_pandas(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,6 @@
+def output_to_console():
+    pass
+
+
+def write_to_file(text, file_path):
+    pass
